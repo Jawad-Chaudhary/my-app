6b296bc1d59be3075817a77e8b2a5aa2cfda281c
Jawad0Chaudhary <uasif412@gmail.com>
2024-10-20 14:40:51 +0500

    nope

---
+++ src/component/button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <main>
+          <button className="text-white bg-green-400 px-4 py-2 hover:bg-green-500" >About us</button>
+    </main>
+    )
+}
